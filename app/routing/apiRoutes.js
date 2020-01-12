5797119b3902bd7c00a7458025d27959dd7cee5a
Jonathan Truong <jjtruong90@gmail.com>
2020-01-12 12:14:32 -0800

    Updates survey.html and apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,9 +2,21 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
     app.get('/api/friends', function(req,res) {
-        res.json(friends)
+    res.json(friends);
     })
+
     app.post('/api/friends', function (req, res) {
+        var totalDifference = 0;
+        var bestMatch= {
+            name="",
+            photo="",
+            friendDifference:1000
+        };
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+
+    
     res.json(friends[0]);
     console.log(friends[0]);
     })
