8d88bdc31ad358757659562c1b7b2eab2f345ac3
Jonathan Truong <jjtruong90@gmail.com>
2020-01-13 20:36:35 -0800

    Updates friends.js,apiRoutes.js, survey.html, and htmlRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 
-var friends = [{
+const friends = [{
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
@@ -14,6 +14,54 @@ var friends = [{
         4,
         1
       ]
-  }
+  },
+  {
+      "name": "James",
+      "photo": "https://gpluseurope.com/wp-content/uploads/Mauro-profile-picture.jpg",
+      "scores": [
+          3,
+          4,
+          5,
+          1,
+          2,
+          3,
+          4,
+          5,
+          1,
+          1,
+      ]
+  },
+  {
+      "name": "Jessica",
+      "photo": "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
+      "scores": [
+          3,
+          4,
+          1,
+          2,
+          3,
+          5,
+          1,
+          2,
+          3,
+          4,
+      ]
+  },
+  {
+      "name": "Corinne",
+      "photo": "http://lookfortheoccasion.com/wp-content/uploads/2017/12/9.jpg",
+      "scores":[
+        1,
+        2,
+        3,
+        4,
+        1,
+        2,
+        3,
+        4,
+        1,
+        2,
+      ]
+  },
   ] 
   module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,21 +3,36 @@ var friends = require("../data/friends");
 module.exports = function(app) {
     app.get('/api/friends', function(req,res) {
     res.json(friends);
-    })
+    });
 
     app.post('/api/friends', function (req, res) {
-        var totalDifference = 0;
-        var bestMatch= {
-            name="",
-            photo="",
-            friendDifference:1000
-        };
-        var userData = req.body;
-        var userName = userData.name;
-        var userScores = userData.scores;
+        const scoreArr = [];
+        let bestMatch = 0;
+        var userScore = req.body.scores;
+        for (var i= 0; i < friends.length; i++) {
+            var scoreDiff = 0;
+            for (var j = 0; j < userScore.length; j++) {
+                scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScore[j])))
+            }
+            scoreArr.push(parseInt(scoreDiff));
+        }
+        for (var i = 0; i <scoreArr.length; i++) {
+            if(scoreArr[i] <= scoreArr[bestMatch]){
+                bestMatch = i;
+            }
+        }
 
-    
-    res.json(friends[0]);
-    console.log(friends[0]);
-    })
+        let idealFriend = friends[bestMatch];
+        res.json(idealFriend);
+
+        console.log(req.body);
+        friends.push(req.body)
+    });
+
+    // app.post('/api/clear', (req,res) => {
+    //     friends.length = [];
+    //     res.json({
+    //         ok:true
+    //     });
+    // });
 }
